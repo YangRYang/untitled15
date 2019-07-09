ebada34a4d36aae75eba3d174c74dadee1b7fedd
heshang <877338044@qq.com>
2019-07-09 13:50:27 +0800

    Initial commit

---
+++ kingadmin/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=20)
+    id = models.IntegerField(primary_key=True)
+
+    ChioceFiled = (
+        (0,'高中'),
+        (1,'大学'),
+        (2,'硕士'),
+        (3,'毕业'),
+    )
+    education = models.IntegerField(choices=ChioceFiled)
+    role = models.ForeignKey('Role',on_delete=models.SET_NULL,null=True)
+class Role(models.Model):
+    RoleChioce = (
+        (0,'领导'),
+        (1,'实习生'),
+        (2,'生力军'),
+    )
+    Role = models.IntegerField(choices=RoleChioce)
+    Power = models.CharField(max_length=20)
+
+
+
+
---
+++ kingadmin/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def psm(request):
+    return render(request,'kingadmin/haha.html')
+
+def ssk(request):
+    return render(request,'king/laozi.html')
