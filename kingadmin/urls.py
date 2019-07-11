db3d626bd0bf4870da94b7c4f30a662d4a92c845
heshang <877338044@qq.com>
2019-07-11 16:13:22 +0800

    more push

--- kingadmin/urls.py
+++ kingadmin/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from kingadmin import views
 urlpatterns=[
-    path('',views.app_index)
+    path('',views.app_index),
+    path('<str:app>/<str:model>/',views.table_obj_list,name = 'table_obj_list'),
+    path('<str:app>/<str:model>/add/',views.table_obj_list_add,name = 'table_obj_list_add')
 ]
--- kingadmin/views.py
+++ kingadmin/views.py
@@ -1,29 +1,39 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from kingadmin.site import site
 from kingadmin.app_setup import kingadmin_auto_discover
+
 kingadmin_auto_discover()
+
+
 # Create your views here.
 def psm(request):
-    return render(request,'kingadmin/haha.html')
+    return render(request, 'kingadmin/haha.html')
+
 
 def ssk(request):
     return render(request, 'kingadmin/laozi.html')
 
+
 def showcustemor(request):
     return render(request, 'kingadmin/base.html')
 
+
 def float(request):
     return render(request, 'kingadmin/float.html')
+
 
 def sitea(request):
     print(dir(site))
     print(site.youwrite)
     return HttpResponse(site)
 
+
 def app_index(request):
-    app_name=[]
-    for app in site.youwrite:
-        app_name.append(app)
+    return render(request, 'kingadmin/app_index.html', {'site': site.youwrite})
 
 
-    return render(request,'kingadmin/app_index.html',{'app_name':app_name})
+def table_obj_list(request, app, model):
+    return render(request,'kingadmin/model.html',{'site':site.youwrite})
+
+def  table_obj_list_add(request, app, model):
+    return render(request,'kingadmin/add.html')
